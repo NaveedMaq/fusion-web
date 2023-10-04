be418cf7d6f20cc86cd3d5e5b669e7c1861af7fd
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-04 17:44:00 +0530

    bugfix: change post to put in save()

--- src/models/User.ts
+++ src/models/User.ts
@@ -48,7 +48,7 @@ export class User {
   save(): void {
     const id = this.get('id');
     if (id) {
-      axios.post(`http://localhost:3000/users/${id}`, this.data);
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
     } else {
       axios.post('http://localhost:3000/users', this.data);
     }
