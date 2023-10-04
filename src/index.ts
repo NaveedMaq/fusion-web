a4a553a23c4055e684cd86c39c42f8f41475a318
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-04 16:32:34 +0530

    feat: add ability to trigger events to user class

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Apple', age: 20 });
 
-user.on('change', () => {});
 user.on('change', () => {
-  console.log('changed');
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
 });
 
-user.on('asdfa', () => {});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user);
+user.trigger('asdfafds');
