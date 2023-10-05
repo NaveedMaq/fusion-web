08debe82b79e14f9a9010af0d5a338f4f1a6d2d2
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-05 23:27:22 +0530

    feat: extract view class

---
+++ src/views/View.ts
@@ -0,0 +1,37 @@
+import { User } from '../models/User';
+
+export abstract class View {
+  abstract eventsMap(): { [key: string]: () => void };
+
+  abstract template(): string;
+
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element: Element): void => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    this.parent.innerHTML = '';
+
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+    this.parent.append(templateElement.content);
+  }
+}
