3fc54a8abdbc7ddfc46f1e264f51ce38af504be1
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-06 01:02:22 +0530

    remove: example code classes

--- src/views/UserList.ts
+++
@@ -1,9 +0,0 @@
-import { User, UserProps } from '../models/User';
-import { CollectionView } from './CollectionView';
-import { UserShow } from './UserShow';
-
-export class UserList extends CollectionView<User, UserProps> {
-  renderItem(model: User, itemParent: Element): void {
-    new UserShow(itemParent, model).render();
-  }
-}
