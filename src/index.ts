f8a55ed2cf0cf025a457dc24aa045ff46d30887e
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-06 01:47:57 +0530

    change package.json file for release

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// Imports from model
+import { ApiSync } from './models/ApiSync';
+import { Attributes } from './models/Attributes';
+import { Collection } from './models/Collection';
+import { Eventing } from './models/Eventing';
+import { Model } from './models/Model';
+
+// Imports from view
+import { CollectionView } from './views/CollectionView';
+import { View } from './views/View';
+
+export {
+  ApiSync,
+  Attributes,
+  Collection,
+  Eventing,
+  Model,
+  CollectionView,
+  View,
+};
