d1e4a05342d70e1b6fbf2dfab8ccba86af50be5f
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-05 23:45:20 +0530

    feat: add data persistance to view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,6 +6,7 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
 
@@ -19,12 +20,17 @@ export class UserForm extends View<User, UserProps> {
     this.model.set({ name });
   };
 
+  onSaveClick = (): void => {
+    this.model.save();
+  };
+
   template(): string {
     return `
       <div>
         <input placeholder=${this.model.get('name')}>
         <button class='set-name'>Change Name</button>
         <button class='set-age'>Set Random Age</button>
+        <button class='save-model'>Save User</button>
       </div>
     `;
   }
