cac6e18ed6b21002c20738c146877dde9f425c54
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-05 15:27:57 +0530

    add model props

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const root = document.getElementById('root');
-if (root) {
-  const userForm = new UserForm(root);
+const parentEl = document.getElementById('root');
+
+const user = User.buildUser({ name: 'NAME', age: 20 });
+if (parentEl) {
+  const userForm = new UserForm(parentEl, user);
   userForm.render();
 }
