4c68daebfe9ff59669ed965f8a300725c2b9254a
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-06 00:41:51 +0530

    Add collection view class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,34 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
 
-const rootEl = document.getElementById('root');
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
 
-const user = User.buildUser({ id: 1 });
-user.fetch();
+// const rootEl = document.getElementById('root');
 
-if (rootEl) {
-  const userEdit = new UserEdit(rootEl, user);
-  userEdit.render();
+// const user = User.buildUser({ id: 1 });
+// user.fetch();
 
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+// if (rootEl) {
+//   const userEdit = new UserEdit(rootEl, user);
+//   userEdit.render();
+
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root element not found');
+// }
+
+const rootUrl = 'http://localhost:3000/users';
+const users = new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+  User.buildUser(json)
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
