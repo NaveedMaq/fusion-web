833da5db2f86d73241cb48fd642bc8ec737c3977
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-05 17:11:09 +0530

    feat: connect model and view

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,7 +1,15 @@
 import { User } from '../models/User';
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
@@ -38,6 +46,8 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
