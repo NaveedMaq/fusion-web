0b0353d93c842e73f4ceb42093f4e652155c2e62
Naveed Maqbool <naveedmaqbool30@gmail.com>
2023-10-05 17:22:39 +0530

    feat: enable tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const parentEl = document.getElementById('root');
+const rootEl = document.getElementById('root');
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
-if (parentEl) {
-  const userForm = new UserForm(parentEl, user);
+
+if (rootEl) {
+  const userForm = new UserForm(rootEl, user);
   userForm.render();
+} else {
+  throw new Error('Root element not found');
 }
